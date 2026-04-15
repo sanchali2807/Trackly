76d48d15755d56dce4d6ed1f2d458374e0643a37
Sanchali <sanchali280705@gmail.com>
2026-04-16 00:54:09 +0530

    sql

--- trello-clone/backend/config/db.js
+++ trello-clone/backend/config/db.js
@@ -1,15 +1,31 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     logging: false,
+//   }
+// );
+
+// module.exports = sequelize;
+
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: "mysql",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
