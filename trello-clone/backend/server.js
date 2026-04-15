e5f31b7a296795c72d705fe8f312115b7e25a8ab
Sanchali <sanchali280705@gmail.com>
2026-04-16 01:14:24 +0530

    sql

--- trello-clone/backend/server.js
+++ trello-clone/backend/server.js
@@ -3,15 +3,20 @@ const sequelize = require("./config/db");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+const models = require("./models"); // store it
 
 async function startServer() {
   try {
     await sequelize.authenticate();
     console.log("✅ DB Connected");
 
-    require("./models");
-    await sequelize.sync(); // later we’ll use migrations
-    console.log("✅ Models Synced");
+    // 🔥 Ensure models are fully initialized
+    Object.keys(models).forEach((model) => {
+      console.log(`Loaded model: ${model}`);
+    });
+console.log(sequelize.models);
+    await sequelize.sync();
+    console.log("✅ Tables created");
 
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
