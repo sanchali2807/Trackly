42dea5ab90c27a46655ee33834087d5b33f58651
Sanchali <sanchali280705@gmail.com>
2026-04-16 13:46:15 +0530

    ds

--- trello-clone/backend/config/db.js
+++ trello-clone/backend/config/db.js
@@ -1,58 +1,26 @@
-// const { Sequelize } = require("sequelize");
-// require("dotenv").config();
-
-// let sequelize;
-
-// if (process.env.DB_URI) {
-//   // 🚀 Railway (production)
-//   console.log(process.env.DB_URI);
-//   console.log("DB CONFIG:", {
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-// });
-// sequelize = new Sequelize(process.env.DB_URI, {
-//   dialect: "mysql",
-//   logging: false,
-// });
-// } else {
-//   // 💻 Local (development)
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: process.env.DB_HOST,
-//       dialect: "mysql",
-//       logging: false,
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
-
-
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
-// let sequelize;
-
-if (process.env.MYSQL_PUBLIC_URL) {
-  console.log("Using Railway PUBLIC DB");
-
-  const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
-  {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+let sequelize;
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
+if (process.env.MYSQLHOST) {
+  // 🚀 Railway
+  console.log("Using Railway DB");
 
- else {
+  sequelize = new Sequelize(
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    {
+      host: process.env.MYSQLHOST,
+      port: process.env.MYSQLPORT,
+      dialect: "mysql",
+      logging: false,
+    }
+  );
+
+} else {
   // 💻 Local
   console.log("Using Local DB");
 
