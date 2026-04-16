60a0725530271f2c60ea2952177d4d462caa3b9e
Sanchali <sanchali280705@gmail.com>
2026-04-16 14:24:46 +0530

    ds

--- trello-clone/backend/routes/cardRoutes.js
+++ trello-clone/backend/routes/cardRoutes.js
@@ -6,7 +6,7 @@ router.post("/", ctrl.createCard);
 // Express matches routes top-to-bottom, so a dynamic route like /:id will treat "move" as the id and capture the request before /move is ever checked.
 // this is called route shadowing
 router.post("/:cardId/labels/:labelId",ctrl.addLabelToCard);
-router.delete("/cards/:cardId/labels/:labelId",ctrl.removeLabelFromCard);
+router.delete("/:cardId/labels/:labelId",ctrl.removeLabelFromCard);
 router.post("/add-member", ctrl.addMemberToCard);
 router.post("/remove-member", ctrl.removeMemberFromCard);
 router.put("/move", ctrl.moveCard);
