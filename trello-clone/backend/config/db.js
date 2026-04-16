5369f0de4329102f2f784dd039ce0df94cb3eca7
Sanchali <sanchali280705@gmail.com>
2026-04-16 14:12:14 +0530

    ds

--- trello-clone/backend/config/db.js
+++ trello-clone/backend/config/db.js
@@ -14,18 +14,18 @@ sequelize = new Sequelize(process.env.DB_URI, {
   dialect: "mysql",
   logging: false,
 });
-} else {
-  // 💻 Local (development)
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: "mysql",
-      logging: false,
-    }
-  );
+// } else {
+//   // 💻 Local (development)
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: process.env.DB_HOST,
+//       dialect: "mysql",
+//       logging: false,
+//     }
+//   );
 }
 
 module.exports = sequelize;
